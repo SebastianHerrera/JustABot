ca1d2fd72226578c4a08143126367adb87b64606
Beleño <sebasbeleno15@gmail.com>
2019-04-16 02:58:44 -0500

    LO QUE PIDIO MRX

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 
 const config = require("./config.json");
 
+
+
 client.on("ready", () => {
     // Este es el evento que se ejecuta cuando el bot es iniciado con exito
     console.log(`Bot iniciado, con ${client.users.size} usuarios, en ${client.channels.size} canales de ${client.guilds.size} servers.`); 
@@ -16,6 +18,11 @@ client.on("guildCreate", guild => {
     //client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
+//EL MALDITO MENSAJE DE Mr.RX2kayer 
+//NO LO VOY A EXPLICAR, COMAN KK >:c
+client.on('guildMemberAdd', member => {
+    member.guild.channels.get('536955514559397899').send(`Bienvenido al server! ${member.user}, Por favor revisa las reglas antes de continuar. Nos vamos ;)`); 
+});
 client.on("message", async message =>{
 
     
@@ -34,7 +41,7 @@ client.on("message", async message =>{
     }
     
     //Dividimos el mensaje en dos, la orden y los parametros.
-    
+
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
@@ -45,6 +52,24 @@ client.on("message", async message =>{
         m.edit(`Pong! La latencia es de ${m.createdTimestamp - message.createdTimestamp}ms, La latencia de la API es de: ${Math.round(client.ping)}ms`);
     
     }
+
+    //Borra los mensajes en un numero expecifico dado por el usuario
+    if (command === "purga") {
+        
+        const deleteCount = parseInt(args[0], 10);
+
+        if (!deleteCount || deleteCount < 2 || deleteCount > 100) {
+            return message.reply("Por davor indique un numero de 2 a 100, que serán los mensajes que desea eliminar");
+        }
+        
+        const fetched = await message.channel.fetchMessages({limit: deleteCount});
+
+        message.channel.bulkDelete(fetched).catch(error => message.reply(`No pude eliminar los mensajes, porque: ${error}`));
+
+    }
+
+
+
 });
 
 client.login(config.token);
