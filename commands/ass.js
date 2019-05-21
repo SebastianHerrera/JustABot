f5a608a2bbabd8b1b4dc8e13e9a599619b3ed79f
Beleño <sebasbeleno15@gmail.com>
2019-05-20 20:49:17 -0500

    arreglo del bug en heroku del comando ass

--- commands/ass.js
+++ commands/ass.js
@@ -1,6 +1,8 @@
 const Discord = module.require("discord.js");
 const request = require('snekfetch');
 const fs = require("fs")
+const superagent = require('superagent')
+
 
 module.exports.run = async (client, message, args) => {
     var max = 5511;
@@ -10,11 +12,10 @@ module.exports.run = async (client, message, args) => {
     if (!message.channel.nsfw) {
         message.channel.send(":underage: Hey! No puedes usar este comando fuera del canal nsfw. Para cochinadas, por favor ve a el.")
     } else {
-        var randomname = Math.floor(Math.random() * (99999999999999999999 - 11111111111111111111 + 0)) + 11111111111111111111;
-        request.get("http://media.obutts.ru/butts_preview/0" + MathLoL + ".jpg").then(r => {
-            fs.writeFile(`ass.jpg`, r.body)
-            message.channel.sendFile(r.body)
-            fs.unlink(`./ass.jpg`)
-        })
+        superagent.get('https://nekobot.xyz/api/image')
+        .query({ type: 'ass'})
+        .end((err, response) => {
+          message.channel.send({ file: response.body.message });
+        });
     }
 }
