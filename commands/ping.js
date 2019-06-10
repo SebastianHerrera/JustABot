86c318ce22f832f9c62a860cedb2efcd165612a3
Beleño <sebasbeleno15@gmail.com>
2019-06-09 23:07:38 -0500

    ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,17 @@
 const Discord = module.require("discord.js");
 
-module.exports.run = async (client, message, args) => {
-    await message.channel.send(`${Date.now() - message.createdTimestamp}ms`);
+module.exports.run =  (client, message, args) => {
+    let ping = Math.floor(message.client.ping);
+
+    message.channel.send(":ping_pong: Pong!")
+    .then(m => {
+        const embed = new Discord.RichEmbed()
+        .setDescription(`:incoming_envelope: Ping Mensajes: \`${m.createdTimestamp - message.createdTimestamp} ms\`\n:satellite_orbital: Ping DiscordAPI: \`${ping} ms\``)
+        .setColor(0x00AE86)
+                    
+        m.edit(embed);
+
+    });
 }
 
 module.exports.help = {
